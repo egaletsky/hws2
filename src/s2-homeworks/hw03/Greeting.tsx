aea5f72da477c703552c5e1145e41f7298a1e78d
egaletsky <galetsky@gmail.com>
2022-10-28 20:16:32 +0700

    HW3 deploy 2

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = !error?s.errorInput:s.input // need to fix with (?:)+++++
+    const inputClass = error?s.errorInput:s.input // need to fix with (?:)+++++
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
