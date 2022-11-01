18e430cfd3b5a7ce55caf57ca5aee175d3e724aa
egaletsky <galetsky@gmail.com>
2022-11-01 22:12:57 +0700

    HW4 init

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -54,6 +54,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         + (error ? ' ' + s.errorInput : ' ' + s.superInput)
         + (className ? ' ' + s.className : '') // задача на смешивание классов
 
+
     return (
         <div className={s.inputWrapper}>
             <input
