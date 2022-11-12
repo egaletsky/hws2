380d2cb7fe784522807dfad0930ff114753b810d
egaletsky <galetsky@gmail.com>
2022-11-12 11:45:05 +0700

    HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,6 +12,8 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+    debugger
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +33,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+
+                         className={({isActive})=>isActive?s.active:''} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive})=>isActive?s.active:''} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive})=>isActive?s.active:''} // делает студент
                     >
                         Junior Plus
                     </NavLink>
