6345851e38a3987430f9c07994560769f02f368e
egaletsky <galetsky@gmail.com>
2022-11-16 21:21:23 +0700

    HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,7 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-        restoreState<string>('hw6-editable-span-value', '')
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
