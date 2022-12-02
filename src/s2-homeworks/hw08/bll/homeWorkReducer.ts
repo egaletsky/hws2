6c1a70bb29a1e62ae62410f37d7e33dfb1126f12
egaletsky <galetsky@gmail.com>
2022-12-02 19:46:19 +0700

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let result: UserType[] = []
+            if (action.payload === 'up') {
+                result = state.sort((a, b) => a.name > b.name ? -1 : 1)
+            }
+            if (action.payload === 'down') {
+                result = state.sort((a, b) => a.name <= b.name ? -1 : 1)
+            }
+            return result
 
-            return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el=> el.age>action.payload)
         }
         default:
             return state
