894917817d440b79f05dd7106cf59889b1096dab
egaletsky <galetsky@gmail.com>
2022-12-18 14:15:18 +0700

    HW10

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HW10 from '../../hw10/HW10';
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
@@ -7,7 +8,7 @@ import React from 'react'
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
+            <HW10 />
             {/*<HW11 />*/}
             {/*<HW12 />*/}
             {/*<HW13 />*/}
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,14 @@
+
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
