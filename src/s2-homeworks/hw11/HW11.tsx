8a3d36f51fa9430a9bd865c78486d27dedf50aed
egaletsky <galetsky@gmail.com>
2022-12-22 18:51:39 +0700

    HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState, saveState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -15,8 +15,21 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number|number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+
+        if(Array.isArray(value)){
+            setValue1(value[0])
+            setValue2(value[1])
+
+            saveState('hw11-value1',value[0])
+            saveState('hw11-value2',value[1])
+
+        }
+        if (typeof value === 'number'){
+            setValue1(value)
+            saveState('hw11-value1',value)
+        }
     }
 
     return (
@@ -30,6 +43,12 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
+                            value={value1}
+                            step={1}
+                            marks
+                            min={0}
+                            max={100}
+                            onChange={(event,value)=> change(event,value)}
 
                         />
                     </div>
@@ -38,6 +57,12 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
+                            value={[value1,value2]}
+                            step={1}
+                            marks
+                            min={0}
+                            max={100}
+                            onChange={(event,value)=> change(event,value)}
 
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
